4fe82bc0f457ed79f16a617030346e438dce838a
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2026-01-17 22:30:58 +0100

    feat: enhance logging system by adding default parameters for logRequest and logResponse methods

--- app/core/logger/logsSystem.php
+++ app/core/logger/logsSystem.php
@@ -3,18 +3,17 @@
 namespace App\Core\Logger;
 
 use App\Core\Http\Request;
-use App\Core\Logger\LoggerFactory;
 
 class LogsSystem {
     public function __construct(private LoggerFactory $loggerFactory, private Request $request) {}
 
-    public function logResponse(array $additionalInfo, int $status = 200){
+    public function logResponse(array $additionalInfo = [], int $status = 200){
         $info = ["status" => $status, ...$additionalInfo];
         $logger = $this->loggerFactory->http();
         $logger->info("Request sent", $info);
     }
 
-    public function logRequest(array $additionalInfo){
+    public function logRequest(array $additionalInfo = []){
         $info = ["method" => $this->request->getRequestType(), "uri" => $this->request->getClearPath(), "body" => $_POST, "queries" => $_GET, ...$additionalInfo];
         $logger = $this->loggerFactory->http();
         $logger->info("Request received", $info);
--- app/core/resolvers/routeResolver.php
+++ app/core/resolvers/routeResolver.php
@@ -3,13 +3,16 @@
 namespace App\Core\Resolvers;
 
 use App\Core\Container\Container;
+use App\Core\Logger\LogsSystem;
 
 class RouteResolver{
-    public function __construct(private MethodResolver $methodResolver) {}
+    public function __construct(private MethodResolver $methodResolver, private LogsSystem $logsSystem) {}
 
     public function resolveRoute(array $routeInfo, array $params){
+        $this->logsSystem->logRequest();
         $controller = $routeInfo["controller"];
         $method = $routeInfo["method"];
+
     
         $args = $this->methodResolver->resolveParams($controller, $method);
         $params = [...$params, ...$args];
